b80ced23a35c82fd4857a74ad1bed21c2f908de1
Sofia Carballo <sofia.carballo@live.com>
2020-05-09 19:56:10 +0200

    Added components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GreetingComponent } from './greeting.component';
+import { NameComponent } from './name/name.component';
+import { ListComponent } from './list/list.component';
+import { SearchstudentComponent } from './searchstudent/searchstudent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingComponent,
+    NameComponent,
+    ListComponent,
+    SearchstudentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
